f441c360b173fd7dcfe652a3c7808e7d4dad5741
Mariell <mariell_ham@dlsl.edu.ph>
2025-05-23 23:41:16 +0800

    etc

--- clinic-app/src/pages/userview/userCurrentAppointments.tsx
+++ clinic-app/src/pages/userview/userCurrentAppointments.tsx
@@ -1,31 +1,50 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, Platform, TouchableOpacity, FlatList } from 'react-native';
+import React from 'react';
+import { View, Text, FlatList, SafeAreaView } from 'react-native';
 import { useGlobalContext } from '../../context/globalcontext';
 import { getGlobalStyles } from '../../styles/globalstyles';
 import { Props } from '../../navigator/props';
 
-const UserCurrentAppointments: React.FC<Props> = ({ navigation }) => {
+const UserCurrentAppointments: React.FC<Props> = () => {
   const { reservations, user } = useGlobalContext();
   const styles = getGlobalStyles();
-  return (
-    <View>
-      <Text>
-        Current Appointments
-      </Text>
 
-      <FlatList
-        data={reservations.filter(r => r.accId === user!.id && r.status != 'completed')}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <View style={styles.appointmentContainer}>
-            <Text>Date: {item.date}</Text>
-            <Text>Time: {item.time}</Text>
-            <Text>Reason: {item.reason}</Text>
-            <Text>Status: {item.status}</Text>
-          </View>
-        )}
-      />
-    </View >
+  if (!user) return null;
+
+  const currentReservations = reservations.filter(
+    r => r.accId === user.id && r.status !== 'completed'
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.header}>Current Appointments</Text>
+
+      {currentReservations.length === 0 ? (
+        <Text style={styles.noReservations}>No current appointments found.</Text>
+      ) : (
+        <FlatList
+          contentContainerStyle={styles.scrollContent}
+          data={currentReservations}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <View style={styles.card}>
+              <Text style={styles.name}>Date: {item.date}</Text>
+              <Text style={styles.detail}>Time: {item.time}</Text>
+              <Text style={styles.detail}>Reason: {item.reason}</Text>
+              <Text
+                style={[
+                  styles.status,
+                  item.status === 'approved'
+                    ? styles.approvedStatus
+                    : styles.pendingStatus,
+                ]}
+              >
+                Status: {item.status}
+              </Text>
+            </View>
+          )}
+        />
+      )}
+    </SafeAreaView>
   );
 };
 

7138bfd1499d06c19505f18df2c605fbdb1f6c24
Num-Three <3abanilla@gmail.com>
2025-05-23 23:25:53 +0800

    added user context in calendar picker

--- clinic-app/src/pages/userview/userCurrentAppointments.tsx
+++ clinic-app/src/pages/userview/userCurrentAppointments.tsx
@@ -7,14 +7,36 @@ import { Props } from '../../navigator/props';
 const UserCurrentAppointments: React.FC<Props> = ({ navigation }) => {
   const { reservations, user } = useGlobalContext();
   const styles = getGlobalStyles();
+  const [category, setCategory] = useState('Current');
+  const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+
+  if (!user) {
+    return
+  }
   return (
     <View>
-      <Text>
-        Current Appointments
-      </Text>
+      <TouchableOpacity onPress={() => setCategory('pending')}>
+                      Pending
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => setCategory('approved')}>
+                      Approved
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => setCategory('rejected')}>
+                      Rejected
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => setCategory('cancelled')}>
+                      Cancelled
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => setCategory('completed')}>
+                      Completed
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => setCategory('missed')}>
+                      Missed
+                  </TouchableOpacity>
+                  <Text>{capitalize(category)} Appointments</Text>
 
       <FlatList
-        data={reservations.filter(r => r.accId === user!.id && r.status != 'completed')}
+        data={reservations.filter(r => r.accId === user!.id && r.status === category)}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
           <View style={styles.appointmentContainer}>
